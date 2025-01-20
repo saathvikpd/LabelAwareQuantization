159224a5e068c47bab13814672c6687a41cf48c0
JessHung323 <jessica.hung0323@outlook.com>
2025-01-20 11:04:02 -0800

    incoprated kl into gpfq for cifar100

---
+++ src/.ipynb_checkpoints/cifar100_subset_generation-checkpoint.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def generate_subset(n_classes, dist_matrix_path = "./cifar100_kl_div_matrix.npy", class_names_path = "./cifar100_class_names.npy", similar_classes = True):
+
+    assert n_classes > 1
+
+    print(f"SIM_CLASSES_TYPE: {type(similar_classes)}")
+
+    kls_cf_matrix = np.load(dist_matrix_path)
+    class_names = np.load(class_names_path)
+    
+    rand_class_ind = np.random.randint(0, class_names.shape[0])
+    rand_class_name = class_names[rand_class_ind]
+    
+    selected_classes = {rand_class_ind}
+    for _ in range(n_classes - 1):
+        if similar_classes:
+            sorted_classes = np.argsort(kls_cf_matrix[list(selected_classes)].mean(axis = 0))
+        else:
+            sorted_classes = np.argsort(kls_cf_matrix[list(selected_classes)].mean(axis = 0))[::-1]
+        i = 0
+        while sorted_classes[i] in selected_classes:
+            i += 1
+        selected_classes.add(sorted_classes[i])
+    
+    selected_classes = list(selected_classes)
+    
+    subset_dists = []
+    subset_classes = []
+    for i in range(len(selected_classes)):
+        c = selected_classes[i]
+        subset_classes += [c]
+        for j in range(i + 1, len(selected_classes)):
+            subset_dists += [kls_cf_matrix[selected_classes[i], selected_classes[j]]]
+    
+    return {"classes": [class_names[class_id] for class_id in subset_classes], "max_dist": max(subset_dists), "avg_dist": np.mean(subset_dists)}
---
+++ src/data_loaders.py
@@ -0,0 +1,87 @@
+import torch
+import torchvision
+from torch.utils.data import Subset, Dataset, DataLoader
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+import random
+import os
+import glob
+import re
+import pickle
+from utils import parse_imagenet_val_labels
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+g = torch.Generator()
+g.manual_seed(0)
+# use above function and g to preserve reproducibility.
+
+class Imagenet(Dataset):
+    """
+    Validation dataset of Imagenet
+    """
+    def __init__(self, data_dir, transform):
+        # we can maybe pput this into diff files.
+        self.Y = torch.from_numpy(parse_imagenet_val_labels(data_dir)).long()
+        self.X_path = sorted(glob.glob(os.path.join(data_dir, 'ILSVRC2012_img_val/*.JPEG')), 
+            key=lambda x: re.search('%s(.*)%s' % ('ILSVRC2012_img_val/', '.JPEG'), x).group(1))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.X_path)
+    
+    def __getitem__(self, idx):
+        img = Image.open(self.X_path[idx]).convert('RGB')
+        y = self.Y[idx] 
+        if self.transform:
+            x = self.transform(img)
+        return x, y
+
+
+def data_loader(ds_name, batch_size, num_workers, classes_of_interest): 
+    """
+    Prepare data loaders
+    """
+    if ds_name == 'CIFAR100':
+        data_dir = '../data'
+
+        transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        train_ds = torchvision.datasets.CIFAR100(root=data_dir, train=True, download=True, 
+            transform=transform_train)
+        
+        test_ds = torchvision.datasets.CIFAR100(root=data_dir, train=False, download=True, 
+            transform=transform_test)
+
+        # Filter the indices of classes of interest
+        def filter_classes(dataset, classes_of_interest):
+            indices = [i for i, (_, label) in enumerate(dataset) if label in classes_of_interest]
+            return Subset(dataset, indices)
+            
+        train_ds_filtered = filter_classes(train_ds, classes_of_interest)
+        test_ds_filtered = filter_classes(test_ds, classes_of_interest)
+        
+        train_dl = DataLoader(train_ds, shuffle=True, batch_size=batch_size,
+                              num_workers=num_workers, worker_init_fn=seed_worker, generator=g)
+        
+        test_dl = DataLoader(test_ds, shuffle=False, batch_size=min(batch_size, 1024),
+                             num_workers=num_workers)
+
+    else:
+        raise Exception('Unkown dataset! This script is intended only for CIFAR100!')
+
+    return train_dl, test_dl 
